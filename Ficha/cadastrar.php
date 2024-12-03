8f510670e43b413513e50676926c2b9a151fb88c
Humberto Petita <humbertopetita@gmail.com>
2024-12-03 20:53:13 -0300

    salvar

--- Ficha/cadastrar.php
+++ Ficha/cadastrar.php
@@ -2,29 +2,46 @@
 
 include("../Classe/Conexao.php");
 
-$nome = isset($_POST["nome"]) ? $_POST["nome"] : NULL;
-$tipo_exercicio = isset($_POST["exercicio"]) ? $_POST["exercicio"] : NULL;
-$grupo_muscular = isset($_POST["grupo"]) ? $_POST["grupo"] : NULL;
+$aluno = isset($_POST["nome"]) ? $_POST["nome"] : NULL;
+$nomeFicha = isset($_POST["nomeFicha"]) ? $_POST["nomeFicha"] : NULL;
+$dia_treino = isset($_POST["dia"]) ? $_POST["dia"] : NULL;
+$nome_exercicio = isset($_POST["exercicio"]) ? $_POST["exercicio"] : NULL;
+$num_series = isset($_POST["series"]) ? $_POST["series"] : NULL;
+$num_repeticoes = isset($_POST["repeticoes"]) ? $_POST["repeticoes"] : NULL;
+$tempo_descanso = isset($_POST["descanso"]) ? $_POST["descanso"] : NULL;
 
 
 $sql = ("INSERT INTO `exercicio` 
     (
-        `nome`, 
-        `tipo_exercicio`, 
-        `grupo_muscular`
+        `aluno`, 
+        `nomeFicha`, 
+        `dia_treino`,
+        `nome_exercicio`, 
+        `num_series`, 
+        `num_repeticoes`,
+        `tempo_descanso`
     ) 
     VALUES 
     (
-        :nome,
-        :tipo_exercicio,
-        :grupo_muscular
+        :aluno,
+        :nomeFicha,
+        :dia_treino,
+        :nome_exercicio,
+        :num_series,
+        :num_repeticoes,
+        :tempo_descanso
 )");
 
 $executar = Db::conexao()->prepare($sql);
 
-$executar->bindValue(":nome", $nome, PDO::PARAM_STR);
-$executar->bindValue(":tipo_exercicio", $tipo_exercicio, PDO::PARAM_STR);
-$executar->bindValue(":grupo_muscular", $grupo_muscular, PDO::PARAM_STR);
+$executar->bindValue(":aluno", $aluno, PDO::PARAM_STR);
+$executar->bindValue(":nomeFicha", $nomeFicha, PDO::PARAM_STR);
+$executar->bindValue(":dia_treino", $dia_treino, PDO::PARAM_STR);
+$executar->bindValue(":nome_exercicio", $nome_exercicio, PDO::PARAM_STR);
+$executar->bindValue(":num_series", $num_series, PDO::PARAM_INT);
+$executar->bindValue(":num_repeticoes", $num_repeticoes, PDO::PARAM_INT);
+$executar->bindValue(":tempo_descanso", $tempo_descanso, PDO::PARAM_INT);
+
 
 $executar->execute();
 
--- Ficha/index.php
+++ Ficha/index.php
@@ -43,78 +43,73 @@
     </div>
 
     <!-- Modal de Cadastro de Exercício -->
-    <form method="POST" action="cadastrar.php">
-        <div class="modal fade" id="userForm" tabindex="-1" aria-labelledby="userFormLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="userFormLabel">Cadastrar Nova Ficha</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    
-                        <div class="modal-body">
-                            <div class="mb-3">
-                                <label for="nomeAluno" class="form-label">Aluno</label>
-                                <input type="text" class="form-control" id="nomeAluno" required>
-                            </div>
-                            <div class="mb-3">
-                                <label for="nomeFicha" class="form-label">Nome da Ficha</label>
-                                <input type="text" class="form-control" id="nomeFicha" required>
-                            </div>
-                            <div class="mb-3">
-                                <label for="diaTreino" class="form-label">Dia do Treino</label>
-                                <select class="form-control" id="diaTreino" required>
-                                    <option value="">Selecione o dia</option>
-                                    <option value="Segunda">Segunda</option>
-                                    <option value="Terça">Terça</option>
-                                    <option value="Quarta">Quarta</option>
-                                    <option value="Quinta">Quinta</option>
-                                    <option value="Sexta">Sexta</option>
-                                    <option value="Sábado">Sábado</option>
-                                </select>
-                            </div>
-                            <div class="mb-3">
-                                <label for="nomeExercicio" class="form-label">Nome do Exercício</label>
-                                <select name="categoriaExercicio" class="form-control" id="nomeExercicio" onchange="mostrarCampos()">
-                                    <option value="">Selecione o exercício</option>
-                                    <?php
-                                        $query = $conexao->query("SELECT id, nome FROM exercicio ORDER BY nome ASC");
-                                        $exercicios = $query->fetchAll(PDO::FETCH_ASSOC);
-
-                                        foreach($exercicios as $option) {
-                                    ?>
-                                        <option value="<?php echo $option['id'] ?>"><?php echo htmlspecialchars($option['nome']) ?></option>
-                                    <?php
-                                        }
-                                    ?>
-                                </select>
-                            </div>
-
-                            <div id="campoSeries" class="mb-3" style="display: none;">
-                                <label for="numeroSeries" class="form-label">Número de Séries</label>
-                                <input type="number" class="form-control" id="numeroSeries" min="2" max="5" required>
-                            </div>
-
-                            <div id="campoRepeticoes" class="mb-3" style="display: none;">
-                                <label for="numeroRepeticoes" class="form-label">Número de Repetições</label>
-                                <input type="number" class="form-control" id="numeroRepeticoes" min="7" max="30" required>
-                            </div>
-
-                            <div id="campoDescanso" class="mb-3" style="display: none;">
-                                <label for="tempoDescanso" class="form-label">Tempo de Descanso</label>
-                                <input type="number" class="form-control" id="tempoDescanso" min="7" max="30" required>
-                            </div>
-
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Fechar</button>
-                            <button type="submit" class="btn btn-success submit">Salvar</button>
-                        </div>
-                 
-                </div>
+<form method="POST" id="myForm" action="cadastrar.php">
+    <div class="modal fade" id="userForm" tabindex="-1" aria-labelledby="userFormLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-header">
+                <h5 class="modal-title" id="userFormLabel">Cadastrar Nova Ficha</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
+            <div class="modal-body">
+                <div class="mb-3">
+                    <label for="nomeAluno" class="form-label">Aluno</label>
+                    <input type="text" name="nome" class="form-control" id="nomeAluno" required>
+                </div>
+                <div class="mb-3">
+                    <label for="nomeFicha" class="form-label">Nome da Ficha</label>
+                    <input type="text" name="nomeFicha" class="form-control" id="nomeFicha" required>
+                </div>
+                <div class="mb-3">
+                    <label for="diaTreino" class="form-label">Dia do Treino</label>
+                    <select class="form-control" name="dia" id="diaTreino" required>
+                        <option value="">Selecione o dia</option>
+                        <option value="Segunda">Segunda</option>
+                        <option value="Terça">Terça</option>
+                        <option value="Quarta">Quarta</option>
+                        <option value="Quinta">Quinta</option>
+                        <option value="Sexta">Sexta</option>
+                        <option value="Sábado">Sábado</option>
+                    </select>
+                </div>
+                <div class="mb-3">
+                    <label for="nomeExercicio" class="form-label">Nome do Exercício</label>
+                    <select name="exercicio" class="form-control" id="nomeExercicio" onchange="mostrarCampos()">
+                        <option value="">Selecione o exercício</option>
+                        <?php
+                            $query = $conexao->query("SELECT id, nome FROM exercicio ORDER BY nome ASC");
+                            $exercicios = $query->fetchAll(PDO::FETCH_ASSOC);
+
+                            foreach($exercicios as $option) {
+                        ?>
+                            <option value="<?php echo $option['id'] ?>"><?php echo htmlspecialchars($option['nome']) ?></option>
+                        <?php
+                            }
+                        ?>
+                    </select>
+                </div>
+
+                <div id="campoSeries" class="mb-3" style="display: none;">
+                    <label for="numeroSeries" class="form-label">Número de Séries</label>
+                    <input type="number" name="series" class="form-control" id="numeroSeries" min="2" max="5" required>
+                </div>
+
+                <div id="campoRepeticoes" class="mb-3" style="display: none;">
+                    <label for="numeroRepeticoes" class="form-label">Número de Repetições</label>
+                    <input type="number" name="repeticoes" class="form-control" id="numeroRepeticoes" min="7" max="30" required>
+                </div>
+
+                <div id="campoDescanso" class="mb-3" style="display: none;">
+                    <label for="tempoDescanso" class="form-label">Tempo de Descanso</label>
+                    <input type="number" name="descanso" class="form-control" id="tempoDescanso" min="7" max="30" required>
+                </div>
+                <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Fechar</button>
+                <button type="submit" class="btn btn-success submit">Salvar</button>    
+                </div>
+            </div>   
         </div>
-    </form>
+    </div>
+</form>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="script.js"></script>
     
